8c419f713b16011a36e301b916b11c25f2c68d03
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-06-07 12:14:15 -0400

    commit tree bd0ca63492b99d9bc2795c15abdb21575eaa0f46

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  if (distanceInFeet <= 400) {
+    return "This one is on me!"
+  } else if (distanceInFeet <= 2000) {
+    return "That will be twenty bucks."
+  } else if (distanceInFeet <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (distanceInFeet > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  let reply
+  cityName === ('NYC') ? (reply = "Ok, sounds good.") : (reply = "No go.")
+  return reply
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount) {
+    case "generous" :
+      return "Thank you so much."
+    case "not as generous" :
+      return "Thank you."
+    default : return "Bye."
+  }
 }
